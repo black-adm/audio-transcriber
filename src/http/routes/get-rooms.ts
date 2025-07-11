5fb6b04ac9f6286d814c6f90299ba5404068397b
black-adm <mattheuscontato17@gmail.com>
2025-07-10 22:14:21 -0300

    feat: create question and questions count in get all rooms

--- src/http/routes/get-rooms.ts
+++ src/http/routes/get-rooms.ts
@@ -1,6 +1,7 @@
-import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod'
-import { db } from '../../db/connection.ts'
-import { schema } from '../../db/schema/index.ts'
+import { count, eq } from 'drizzle-orm';
+import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod';
+import { db } from '../../db/connection.ts';
+import { schema } from '../../db/schema/index.ts';
 
 export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
   app.get('/rooms', async () => {
@@ -8,12 +9,16 @@ export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
       .select({
         id: schema.rooms.id,
         name: schema.rooms.name,
+        questionsCount: count(schema.questions.id),
+        createdAt: schema.rooms.createdAt
       })
       .from(schema.rooms)
-      .orderBy(schema.rooms.createdAt)
+      .leftJoin(schema.questions, eq(schema.questions.roomId, schema.rooms.id))
+      .groupBy(schema.rooms.id)
+      .orderBy(schema.rooms.createdAt);
 
     return {
-      rooms: results
-    }
-  })
-}
+      rooms: results,
+    };
+  });
+};
